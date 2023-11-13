3e8163abb11edc9da5d7808c0578ee93e79e9c78
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-11-13 14:37:38 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { PageSimpleForm } from "./pages/PageSimpleForm.tsx";
-import { PageInfo } from "./pages/PageInfo.tsx";
+import { PageEmployees } from "./pages/PageEmployees.tsx";
 import { PageAbout } from "./pages/PageAbout.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 
@@ -18,8 +18,8 @@ const router = createBrowserRouter([
 				element: <PageSimpleForm />
 			},
 			{
-				path: "info",
-				element: <PageInfo />,
+				path: "employees",
+				element: <PageEmployees />,
 			},
 			{
 				path: "about",
